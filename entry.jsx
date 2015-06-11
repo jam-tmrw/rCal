974edde8cd427239a4c7e800b4a70753dc31b003
jam-tmrw <jameshamilton@gmail.com>
2015-06-11 09:41:10 -0700

    fixing up entry file

--- entry.jsx
+++ entry.jsx
@@ -1,4 +1,4 @@
 var React = require('react');
 var Calendar = require('./src/components/calendar');
 
-React.render(<Calendar/>, document.body);
+React.render(<Calendar rows={6} />, document.getElementById('app'));
